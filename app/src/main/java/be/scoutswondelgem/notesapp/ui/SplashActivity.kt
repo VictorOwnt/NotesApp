e67fc555984d3adf550ee152e270a18aeb576daf
Victor Van hulle <vanhulle.victor@gmail.com>
2020-08-13 19:53:01 +0200

    Added Splashscreen

---
+++ app/src/main/java/be/scoutswondelgem/notesapp/ui/SplashActivity.kt
@@ -0,0 +1,28 @@
+package be.scoutswondelgem.notesapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    private val handler = Handler()
+
+    private val runnable = Runnable {
+        if(!isFinishing) {
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        handler.postDelayed(runnable, 2000)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        handler.removeCallbacks(runnable)
+    }
+}
